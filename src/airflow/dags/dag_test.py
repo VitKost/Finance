24d28609591022e12f8d0eb1b10c835aa2909551
VitKost <vi_ko97@mail.ru>
2023-12-20 13:49:02 +0400

    #6 Dag with file operation

--- src/airflow/dags/dag_test.py
+++ src/airflow/dags/dag_test.py
@@ -28,14 +28,12 @@ myDag = DAG(
 task1 = PythonOperator(
     task_id='print_name',
     python_callable=step1,
-    op_args=['Sam'],
     dag=myDag,
     )
 
 task2 = PythonOperator(
     task_id='print_age',
     python_callable=step2,
-    op_kwargs={'bday': '2004-09-15'},
     dag=myDag,
     )
 
--- src/airflow/dags/dag_test_main.py
+++ src/airflow/dags/dag_test_main.py
@@ -9,52 +9,34 @@ from import_for_dags import *
 from src.tools.airflow_sensors import FileSensorWithMask
 import os
 import glob
+import shutil
 
 
 file_sensing_task = FileSensor(task_id='sense_the_csv',
                                    filepath=INCOMING_MESSAGES_PATH + '/*_CSV_LOAD_*.csv',
                                    poke_interval=10)
 
-def step1(name: str, ti):
-    print(f"Hello, my name is {name}!")
+def step1(ti):
+    print(f"Start to search files in incoming folder")
     files = glob.glob(INCOMING_MESSAGES_PATH + '/*_CSV_LOAD_*.csv')
     files.sort(key = os.path.getmtime)
     print(files)
     ti.xcom_push(key='csv_file_name', value=files[0])
 
-def step2(bday: str):
-    birthdate: datetime = datetime.strptime(bday, '%Y-%m-%d')
-    age: int = relativedelta(datetime.now(), birthdate).years
-    print(f"I am {age} this year.")
+def step2(ti):
+    print(f"Start to take data from a file and load in DB")
+    file_name = ti.xcom_pull(key='csv_file_name')
+    print(file_name)
+    print("Loading to DB...")
+    #TODO DB load process
 
-def step3():
-    dow: str = calendar.day_name[date.today().weekday()]
-    print(f"Today is {dow}.")
+def step3(ti):
+    print(f"Moving file to processed folder")
+    file_name = ti.xcom_pull(key='csv_file_name')
+    shutil.move(file_name, file_name.replace('/incoming/', '/processed/'))
 
-def getArgs():
-    parser = argparse.ArgumentParser(description="Get user input.")
-    parser.add_argument(
-        "-n",
-        "--name",
-        action="store",
-        required=True,
-        type=str,
-        help="Enter name in string.",
-    )
-    parser.add_argument(
-        "-bd",
-        "--birthdate",
-        action="store",
-        required=True,
-        type=str,
-        help="Enter birthdate in yyyy-mm-dd format.",
-    )
-    return parser.parse_args()
+
+
 
 if __name__ == "__main__":
-
-    arg = getArgs()
-
-    step1(name=arg.name)
-    step2(bday=arg.birthdate)
-    step3()
+    pass
